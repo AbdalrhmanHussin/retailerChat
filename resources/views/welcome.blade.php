ccbe939b9e0f603526975f4a87c37cbe1c4cbc5f
abdalrhman hussin <abdalrhmanhussin44@gmail.com>
2021-10-03 20:45:44 +0200

    psuh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="{{ secure_asset('images/system/logo.svg') }}" sizes="any">
+    <link rel="icon" href="{{ asset('images/system/logo.svg') }}" sizes="any">
     <title>Retailer</title>
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('css/generals.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/generals.css') }}" rel="stylesheet">
     
 </head>
 
