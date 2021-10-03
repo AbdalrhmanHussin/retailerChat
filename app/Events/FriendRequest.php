b75c6f97eb1df5dada6b95f7549ba1f7359007a2
abdalrhman hussin <abdalrhmanhussin44@gmail.com>
2021-10-03 22:33:26 +0200

    test

--- app/Events/FriendRequest.php
+++ app/Events/FriendRequest.php
@@ -12,7 +12,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class FriendRequest implements ShouldBroadcastNow
+class FriendRequest implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
